31d71efd4346bc2b4ce592a644adf5a3ff5a2336
ksklaura <ksklaura@gmail.com>
2022-11-20 16:15:02 +0900

    22.11.20 16:15

--- Vuejs/vue30컴포넌트실습/comprj01/src/router/index.js
+++ Vuejs/vue30컴포넌트실습/comprj01/src/router/index.js
@@ -13,7 +13,16 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/3103',
+    name: '3103',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Vue3103View.vue'),
   },
 ];
 
