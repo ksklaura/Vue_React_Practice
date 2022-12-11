40135e58cb77f026235374dee17a712099e2807f
ksklaura <ksklaura@gmail.com>
2022-12-11 13:36:38 +0900

    22.12.11 13:35

---
+++ React/rt20.create-reate-project/reactprj01/src/containers/styled/CompStyle.js
@@ -0,0 +1,47 @@
+// 단축키 : rfc
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+  useReducer,
+  Fragment,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const StyledCompStyle = styled.div`
+  /* styled 설정. https://styled-components.com/docs/basics#adapting-based-on-props */
+  .App {
+    display: inline-block;
+    background-color: gray;
+    border: 10px solid black;
+    height: 63px;
+    width: 300px;
+  }
+`;
+
+function CompStyle({ ...props }) {
+  return (
+    <StyledCompStyle>
+      <h2>styled-components 로 만든</h2>
+      <div className="App">styled-components 스타일로 만든</div>
+    </StyledCompStyle>
+  );
+}
+
+CompStyle.propTypes = {
+  // props의 프로퍼티 타입 설정. https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+  // 인자명: PropTypes.func.isRequired,
+  // 인자명: PropTypes.arrayOf(PropTypes.object),
+};
+CompStyle.defaultProps = {
+  // props의 디폴트 값 설정. https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+  // 인자명: () => {},
+  // 인자명: [],
+};
+
+export default React.memo(CompStyle); // React.memo()는 props 미변경시 컴포넌트 리렌더링 방지 설정
