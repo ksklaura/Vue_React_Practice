23196392461a1df688953db424804b7acbef9041
ksklaura <ksklaura@gmail.com>
2022-12-11 13:53:34 +0900

    22.12.11 13:50

--- React/rt20.create-reate-project/reactprj01/src/containers/styled/CompStyle.js
+++ React/rt20.create-reate-project/reactprj01/src/containers/styled/CompStyle.js
@@ -22,6 +22,33 @@ const StyledCompStyle = styled.div`
     height: 63px;
     width: 300px;
   }
+
+  .circle {
+    width: 5rem;
+    height: 5rem;
+    background: black;
+    border-radius: 50%;
+    margin: auto;
+  }
+`;
+
+// 단축키 : styled
+// import styled, { css } from 'styled-components';
+const StyledCircle = styled.div`
+  /* https://styled-components.com/docs/basics#adapting-based-on-props */
+    width: 5rem;
+    height: 5rem;
+    background: ${(props) => props.color || 'pink'};
+    border-radius: 50%;
+    margin: auto;
+
+    ${(props) =>
+      props.huge &&
+      css`
+        width: 10rem;
+        height: 10rem;
+      `}
+  }
 `;
 
 function CompStyle({ ...props }) {
@@ -29,6 +56,18 @@ function CompStyle({ ...props }) {
     <StyledCompStyle>
       <h2>styled-components 로 만든</h2>
       <div className="App">styled-components 스타일로 만든</div>
+      <hr />
+
+      <div className="circle"></div>
+      <hr />
+
+      <StyledCircle></StyledCircle>
+      <hr />
+
+      <StyledCircle color={'red'}></StyledCircle>
+      <hr />
+
+      <StyledCircle color={'blue'} huge={true}></StyledCircle>
     </StyledCompStyle>
   );
 }
