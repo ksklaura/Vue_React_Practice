0d798d452d8f08ee10ab690f22f91947c8df0167
ksklaura <ksklaura@gmail.com>
2022-12-11 18:12:28 +0900

    22.12.11 18:10 FINAL

--- React/rt20.create-reate-project/reactprj01/src/containers/todo/TodoContainer.js
+++ React/rt20.create-reate-project/reactprj01/src/containers/todo/TodoContainer.js
@@ -16,6 +16,7 @@ import { BrowserRouter, Routes, Route, NavLink, useParams, useLocation, useHisto
 import TodoHeader from './components/TodoHeader';
 import TodoFooter from './components/TodoFooter';
 import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
 
 const StyledTodoContainer = styled.div`
   /* styled 설정. https://styled-components.com/docs/basics#adapting-based-on-props */
@@ -28,112 +29,15 @@ const StyledTodoContainer = styled.div`
   button {
     border-style: groove;
   }
-
-  input {
-    border-style: groove;
-    width: 200px;
-  }
-
-  .shadow {
-    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
-  }
-
-  input:focus {
-    outline: none;
-  }
-
-  .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
-  }
-
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
-  }
-
-  .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478fb, #8763fb);
-    display: inline-block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
-  }
-
-  .addBtn {
-    color: white;
-    vertical-align: middle;
-  }
-
-  .closeModalBtn {
-    color: #62acde;
-  }
-
-  .modal-mask {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: table;
-    transition: opacity 0.3s ease;
-  }
-
-  .modal-wrapper {
-    display: table-cell;
-    vertical-align: middle;
-  }
-
-  .modal-container {
-    width: 300px;
-    margin: 0px auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-    transition: all 0.3s ease;
-    font-family: Helvetica, Arial, sans-serif;
-  }
-
-  .modal-header h3 {
-    margin-top: 0;
-    color: #62acde;
-  }
-
-  .modal-body {
-    margin: 20px 0;
-  }
-
-  .modal-default-button {
-    float: right;
-  }
-
-  .modal-enter {
-    opacity: 0;
-  }
-
-  .modal-leave-active {
-    opacity: 0;
-  }
-
-  .modal-enter .modal-container,
-  .modal-leave-active .modal-container {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
-  }
 `;
 
 function TodoContainer({ ...props }) {
   // useState 를 사용한 컴포넌트의 상태값 설정
   const [todoItems, setTodoItems] = useState([
     { id: 1, todo: '영화보기', done: false },
-    { id: 2, todo: '주말 산책', done: true },
-    { id: 3, todo: 'ES6 학습', done: false },
-    { id: 4, todo: '잠실 야구장', done: false },
+    { id: 2, todo: '산책하기', done: true },
+    { id: 3, todo: 'ES6 학습하기', done: false },
+    { id: 4, todo: '월드컵 보기', done: false },
   ]); // 상태값이 기본타입인 경우
 
   // ref 만들기.
@@ -224,6 +128,35 @@ function TodoContainer({ ...props }) {
     ],
   );
 
+  const callbackAddTodo = useCallback(
+    (value) => {
+      // ap03-11.객체배열.html 참조
+      // map과 reduce 를 사용하여 max id 구하기 ==> newid 만들기
+      // todoItems 추가할 객체 만들기
+      // 배열에 추가. todoItems = [...todoItems, newTodo];
+      const maxid =
+        todoItems &&
+        todoItems
+          .map((item) => item.id) // [1,2,3,4]
+          .reduce((pvalue, cvalue) => {
+            // 큰값 반환하기
+            if (pvalue > cvalue) return pvalue;
+            else return cvalue;
+          }, 0); // 최대값 찾기
+
+      const newTodo = {
+        id: maxid + 1,
+        todo: value,
+        done: false,
+      };
+
+      // todoItems.push(newItem);
+      // todoItems = [...todoItems, newTodo];
+      setTodoItems([...todoItems, newTodo]); // todoItems = [...todoItems, newTodo];
+    },
+    [todoItems],
+  );
+
   // 이벤트 핸들러 작성.
   const handler = (e) => {
     // 이벤트 핸들러는 화살표 함수로 만든다
@@ -238,29 +171,7 @@ function TodoContainer({ ...props }) {
         <TodoHeader></TodoHeader>
 
         {/* <!-- TodoInput --> */}
-        <div className="inputBox shadow">
-          <input type="text" placeholder="Type what you have to do" />
-          <span className="addContainer">
-            <i aria-hidden="true" className="addBtn fas fa-plus"></i>
-          </span>
-
-          <div className="modal-mask" style={{ display: 'none' }}>
-            <div className="modal-wrapper">
-              <div className="modal-container">
-                <div className="modal-header">
-                  <h3 slot="header">경고</h3>
-                </div>
-
-                <div className="modal-footer">
-                  <span>
-                    할 일을 입력하세요.
-                    <i className="closeModalBtn fas fa-times" aria-hidden="true"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <TodoInput callbackAddTodo={callbackAddTodo}></TodoInput>
 
         {/* <!-- TodoList --> */}
         <TodoList
---
+++ React/rt20.create-reate-project/reactprj01/src/containers/todo/components/TodoInput.js
@@ -0,0 +1,252 @@
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+  useReducer,
+  Fragment,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import { BrowserRouter, Routes, Route, NavLink, useParams, useLocation, useHistory, useNavigate } from 'react-router-dom';
+
+const StyledTodoInput = styled.div`
+  /* styled 설정. https://styled-components.com/docs/basics#adapting-based-on-props */
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478fb, #8763fb);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
+
+  .closeModalBtn {
+    color: #62acde;
+  }
+
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
+  }
+
+  .modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .modal-container {
+    width: 300px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+
+  .modal-header h3 {
+    margin-top: 0;
+    color: #62acde;
+  }
+
+  .modal-body {
+    margin: 20px 0;
+  }
+
+  .modal-default-button {
+    float: right;
+  }
+
+  .modal-enter {
+    opacity: 0;
+  }
+
+  .modal-leave-active {
+    opacity: 0;
+  }
+
+  .modal-enter .modal-container,
+  .modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+`;
+
+function TodoInput({ callbackAddTodo }) {
+  // useState 를 사용한 컴포넌트의 상태값 설정
+  const [isShowModal, setIsShowModal] = useState(true); // 상태값이 기본타입인 경우
+  // true이면 경고문이 나오고, false면 경고문이 나오지 않음!
+
+  // ref 만들기.
+  const refInputTodo = useRef();
+
+  // refIsMounted는 생명주기의 마운트와 업데이트를 구분하기 위한 ref
+  const refIsMounted = useRef(false);
+  useEffect(
+    () => {
+      if (refIsMounted.current) {
+        // 업데이트 될 때마다 실행됨. 여러번. state 가 변경될 때마다
+        // console.log('TodoInput >> componentDidUpdate');
+      } else {
+        // 마운트 완료 후에 실행됨. 한번만. focus 줄때
+        // console.log('TodoInput >> componentDidMount');
+        refIsMounted.current = true;
+      }
+      return () => {
+        // 언마운트 직전에 한번만 실행됨.
+        // console.log('TodoInput >> componentWillUmount');
+      };
+    },
+    [
+      /* 연관배열: 콜백 메서드에서 변경하고자 하는 연관되는 상태(변수)명들을 기술 */
+    ],
+  );
+
+  // callback 메서드 작성. callback 메서드는 부모의 공유 상태값을 변경하기 위해서 사용된다.
+  const callback = useCallback(
+    (param) => {
+      // state 변경
+    },
+    [
+      /* 연관배열: 콜백 메서드에서 변경하고자 하는 연관되는 상태(변수)명들을 기술 */
+    ],
+  );
+
+  // 이벤트 핸들러 작성.
+  const handlerShowModal = (e) => {
+    // 이벤트 핸들러는 화살표 함수로 만든다
+    console.log(e.target);
+    debugger;
+
+    // isShowModal의 useState를 false로 바꿔야 경고문이 없어짐.
+    // isShowModal === true  ===> isShowModal = false
+    // isShowModal === false ===> isShowModal = true
+    // isShowModal = !isShowModal
+    setIsShowModal(!isShowModal);
+  };
+
+  const handlerAddTodo = (e) => {
+    // 이벤트 핸들러는 화살표 함수로 만든다
+    console.log(e.target);
+    debugger;
+
+    // click 이벤트 취소. 버블링 방지
+    e.stopPropagation();
+
+    // input 에 입력된 값을 가져오기. 어떻게 해야 하나? ==> ref이름.current.속성명
+    // value 속성값 가져오기: ref이름.current.value
+    // class 속성값 가져오기: ref이름.current.className
+    const value = refInputTodo.current.value;
+
+    // input 태그에 빈 문자열이나 공백이 입력 되는 경우는 modal 창이 출력되게 하기.
+    if (
+      // 아랫줄은 value === null || value === undefined || value === "" 와 동일한 의미
+      !value ||
+      !value.trim()
+    ) {
+      // isShowModal = true;
+      setIsShowModal(true);
+      return; // 함수 실행을 멈춘다.
+    }
+
+    // 부모 컴포넌트인 TodoContainer 의 callbackAddTodo 메서드 호출
+    callbackAddTodo(value);
+
+    // add 후에 input 태그의 입력 값 지우기.
+    // input의 value 속성에 값을 설정하기. ref이름.current.속성명
+    // value 속성값 가져오기. ref이름.current.value
+    refInputTodo.current.value = '';
+  };
+
+  // JSX로 화면 만들기. 조건부 렌더링: https://ko.reactjs.org/docs/conditional-rendering.html
+  return (
+    <StyledTodoInput>
+      <div className="inputBox shadow">
+        <input
+          type="text"
+          placeholder="Type what you have to do"
+          ref={refInputTodo}
+          onKeyUp={(e) => e.keyCode === 13 && handlerAddTodo(e)}
+        />
+        <span className="addContainer" onClick={handlerAddTodo}>
+          <i aria-hidden="true" className="addBtn fas fa-plus"></i>
+        </span>
+
+        {isShowModal && (
+          <div className="modal-mask">
+            <div className="modal-wrapper">
+              <div className="modal-container">
+                <div className="modal-header">
+                  <h3 slot="header">경고</h3>
+                </div>
+
+                <div className="modal-footer" onClick={handlerShowModal}>
+                  <span>
+                    할 일을 입력하세요.
+                    <i className="closeModalBtn fas fa-times" aria-hidden="true"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </StyledTodoInput>
+  );
+}
+
+TodoInput.propTypes = {
+  // props의 프로퍼티 타입 설정. https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+  // 인자명: PropTypes.func.isRequired,
+  // 인자명: PropTypes.arrayOf(PropTypes.object),
+  callbackAddTodo: PropTypes.func.isRequired,
+};
+TodoInput.defaultProps = {
+  // props의 디폴트 값 설정. https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+  // 인자명: () => {},
+  // 인자명: [],
+  callbackAddTodo: () => {},
+};
+
+export default React.memo(TodoInput); // React.memo()는 props 미변경시 컴포넌트 리렌더링 방지 설정
