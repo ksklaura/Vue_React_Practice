e4f1680512db0195aaa78923f5c873600ad9213a
ksklaura <ksklaura@gmail.com>
2022-12-11 15:42:21 +0900

    22.12.11 15:40

--- React/rt20.create-reate-project/reactprj01/src/App.js
+++ React/rt20.create-reate-project/reactprj01/src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
-import { Routes, Route, NavLink } from 'react-router-dom';
-
-import Home from './components/Home';
-import CompStyle from './containers/styled/CompStyle';
+import PageHome from './pages/PageHome';
 
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/styled">Styled</NavLink>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/styled" element={<CompStyle />} />
-      </Routes>
+      <PageHome></PageHome>
     </div>
   );
 }
